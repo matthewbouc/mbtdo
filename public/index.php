46667618394c6026e42b104f4fb99f92d38066dd
mbouc <74280078+matthewbouc@users.noreply.github.com>
2023-09-07 14:41:05 -0500

    tagged

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 const varies = "new";
+const thisIsNew = "tag_Me";
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
