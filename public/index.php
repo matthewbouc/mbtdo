61bbe981a42b4eeb5c1790aed1dfe14a85fbf1ed
mbouc <74280078+matthewbouc@users.noreply.github.com>
2023-09-07 14:44:11 -0500

    this is the real tag test

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+const realTag = "actual tag";
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
